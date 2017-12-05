46cbc35c91f76f93d506fb54169879fb745eca92
mnika1996 <maknika1996@yandex.ru>
2017-12-05 15:47:08 +0300

    Add files via upload

---
+++ TestVC/TestVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestVC
+//
+//  Created by Вероника on 11.11.17.
+//  Copyright © 2017 Вероника. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestVC/TestVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestVC
+//
+//  Created by Вероника on 11.11.17.
+//  Copyright © 2017 Вероника. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
