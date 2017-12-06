e30ca19ac9d94f4d09cdbfbf628cecf05d2ea402
mnika1996 <maknika1996@yandex.ru>
2017-12-06 16:29:14 +0300

    Add files via upload

---
+++ AnimationTest/AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by Вероника on 21.11.17.
+//  Copyright © 2017 Вероника. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) NSTimer *someRandomTimer;
+
+@end
+
